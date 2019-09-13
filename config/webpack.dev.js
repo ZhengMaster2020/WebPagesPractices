f702d189d84381875a50fb1a32350445684bb938
ZhengMaster2020 <2105195274@qq.com>
2019-09-13 17:23:29 +0800

    finish webpack config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,19 @@
+const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 const dev = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, '../dist'),
