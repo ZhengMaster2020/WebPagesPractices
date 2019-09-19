90d32ad51b89450e7355c57e2a03ef840bc4d3d0
ZhengMaster2020 <2105195274@qq.com>
2019-09-19 16:24:23 +0800

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,59 @@
 <template>
-  <div class="login">
-    <Row type='flex' justify='center' class="login-header">
-      <h2>欢迎登录</h2>
-    </Row>
-    <Row type='flex' justify='center' class="code-row-bg">
-      <col span="18">
-        <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
-            <Form-item prop="user">
-              <Input type="text" v-model="formInline.user" placeholder="Username">
-                  <Icon type="ios-person-outline" slot="prepend"></Icon>
-              </Input>
-            </Form-item><br>
-            <Form-item prop="password">
-              <Input type="password" v-model="formInline.password" placeholder="Password">
-                  <Icon type="ios-lock-outline" slot="prepend"></Icon>
-              </Input>
-            </Form-item><br>
-            <Form-item>
-                <Button type="primary" @click="handleSubmit('formInline')">登录</Button>
-            </Form-item>
-        </Form>
-      </col>
-    </Row>
+  <div>
+    <div class="bg"></div>
+    <div class="login">
+      <Tabs>
+          <Tab-pane label="用户注册">
+             <Row type='flex' justify='center' class="code-row-bg">
+              <col span="18">
+                <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+                  <Form-item prop="user">
+                    <Input type="text" v-model="formInline.user" placeholder="用户名">
+                        <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                  </Form-item><br>
+                  <Form-item prop="password">
+                    <Input type="password" v-model="formInline.password" placeholder="密码">
+                        <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                    </Input>
+                  </Form-item><br>
+                  <Form-item prop="password2">
+                    <Input type="password" v-model="formInline.password" placeholder="确认密码">
+                        <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                    </Input>
+                  </Form-item><br>
+                  <Form-item>
+                      <Button type="primary" @click="handleSubmit('formInline')">登录</Button>
+                      <Button type="primary" @click="handleReset('formInline')">重置</Button>
+                  </Form-item>
+                </Form>
+              </col>
+            </Row>
+          </Tab-pane>
+          <Tab-pane label="登录">
+            <Row type='flex' justify='center' class="code-row-bg">
+              <col span="18">
+                <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+                    <Form-item prop="user">
+                      <Input type="text" v-model="formInline.user" placeholder="Username">
+                          <Icon type="ios-person-outline" slot="prepend"></Icon>
+                      </Input>
+                    </Form-item><br>
+                    <Form-item prop="password">
+                      <Input type="password" v-model="formInline.password" placeholder="Password">
+                          <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                      </Input>
+                    </Form-item><br>
+                    <Form-item>
+                        <Button type="primary" @click="handleSubmit('formInline')">登录</Button>
+                    </Form-item>
+                </Form>
+              </col>
+            </Row>
+          </Tab-pane>
+      </Tabs>
+    </div>
   </div>
-    
 </template>
 <script>
 export default {
@@ -66,10 +96,26 @@ export default {
 </script>
 
 <style scoped>
-.login{
-  margin: 10px;
+.bg{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 600px;
+  background-image: url(https://gtms01.alicdn.com/tps/i1/TB1GTCYLXXXXXcHXpXXcoeQ2VXX-2500-600.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  overflow: hidden;
 }
-.login-header{
-  margin-bottom: 20px;
+.login{
+  position: absolute;
+  top: 120px;
+  right: 120px;
+  margin: 10px;
+  width: 350px;
+  padding: 20px;
+  background-color: rgba(255,255,255,.9);
+  border: 1px solid red
 }
 </style>
